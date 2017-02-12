1178d2fcf345f09912b56ed0f7878e2bebab11cf
hayoc <hayo.ce@gmail.com>
2017-02-12 22:29:09 +0100

    FEAT: Expose Indecision flow

--- src/main/java/hayoc/indecision/setup/Indecision.java
+++ src/main/java/hayoc/indecision/setup/Indecision.java
@@ -1,6 +1,8 @@
 package hayoc.indecision.setup;
 
 import hayoc.indecision.decision.Decision;
+import hayoc.indecision.initialization.Initializer;
+import org.apache.commons.codec.digest.DigestUtils;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,6 +12,15 @@ import java.util.List;
  */
 public class Indecision {
 
+    private Decision decision;
+    private Initializer initializer;
+
+    public Indecision() {
+        Distributor distributor = Distributor.setup();
+        decision = distributor.getDecision();
+        initializer = distributor.getInitializer();
+    }
+
     public static void main(String[] args) {
         new Indecision().execute();
     }
@@ -18,10 +29,27 @@ public class Indecision {
         List<String> options = new ArrayList<>();
         options.add("blah");
         options.add("booh");
-
-        Distributor distributor = Distributor.setup();
-        Decision decision = distributor.getDecision();
         String chosen = decision.decide(options, "hayotest");
         System.out.println(chosen);
+    }
+
+    public String decide(List<String> options, String user) {
+        return decision.decide(options, md5(user));
+    }
+
+    public void updateUserDataWithDecision(List<String> options, String chosen, String user) {
+        decision.update(options, chosen, md5(user));
+    }
+
+    public boolean createUser(String user) {
+        return initializer.createUser(md5(user));
+    }
+
+    public List<String> getOptions(int index) {
+        return initializer.getOptions(index);
+    }
+
+    private String md5(String input) {
+        return DigestUtils.md5Hex(input);
     }
 }
